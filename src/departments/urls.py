2cec104741409ba018326754d078109b23490efd
Andrea Hrelja <andhrelja@hotmail.com>
2020-12-05 16:07:52 +0100

    Departments

--- src/departments/urls.py
+++ src/departments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from . import views
 
 app_name = 'departments'
 urlpatterns = [
-    
+    path("create/", views.create, name='create'),
 ]
--- src/departments/views.py
+++ src/departments/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create(request):
+    return render(request, template_name='departments/create.html')
