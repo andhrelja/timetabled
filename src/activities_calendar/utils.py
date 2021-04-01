5138d85ca7b5f3bcceae7a780c47fb6164fec3e2
Andrea Hrelja <andhrelja@hotmail.com>
2021-04-01 14:47:27 +0200

    Date start fix (activities_calendar.utils.get_first_dayofcurrentweek)

--- src/activities_calendar/utils.py
+++ src/activities_calendar/utils.py
@@ -1,18 +1,24 @@
 from django.utils import timezone
+import calendar
 
 
 def get_first_dayofcurrentweek():
     now = timezone.now()
-    start_date = now.replace(day=(now.day - now.weekday()))
+    new_day = now.day - now.weekday()
+    try:
+        start_date = now.replace(day=new_day)
+    except ValueError: # Day is out of range
+        last_day_of_prev_month = calendar.monthrange(now.year, now.month-1)[1]
+        new_day = last_day_of_prev_month + new_day
+        start_date = now.replace(day=new_day, month=now.month-1)
     return start_date
 
 def get_last_dayofcurrentweek():
     now = timezone.now()
-    new_day = (now.day + (6 - now.weekday()))
+    new_day = now.day + (6 - now.weekday())
     try:
         end_date = now.replace(day=new_day)
     except ValueError: # Day is out of range
-        import calendar
         last_day_of_month = calendar.monthrange(now.year, now.month)[1]
         new_day = new_day - last_day_of_month
         end_date = now.replace(day=new_day, month=now.month+1)
